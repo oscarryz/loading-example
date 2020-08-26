ec1f0747b16cdf5c839639373927bf83de3c3393
oscarryz <oscarryz@gmail.com>
2020-08-25 23:29:24 -0500

    Load users showing skeletons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './index.css';
+import axios from 'axios'
+import User from './User'
+
 
 function App() {
+  const url = 'http://slowwly.robertomurray.co.uk/delay/4000/url/https://reqres.in/api/users?page=2'
+  const [users, setUsers] = useState([null, null, null])
+  useEffect(() => {
+    (async () => {
+      // Get data with a delay
+      const response = await axios.get(url)
+      setUsers(response.data.data)
+    })()
+
+  }, [url])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        users.map(u => {
+          if (u !== null) {
+            u.title = 'Program Manager'
+            u.phone = '(555) 658-1111'
+          }
+          return <User data={u} />
+        })
+      }
     </div>
   );
 }
